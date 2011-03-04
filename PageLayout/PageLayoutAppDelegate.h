e6866b2359ca5ae2282b49f33cb58f4b0196c96e
Andrew Pouliot <andrew@darknoon.com>
2011-03-04 13:21:33 -0800

    Initial Commit

---
+++ PageLayout/PageLayoutAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PageLayoutAppDelegate.h
+//  PageLayout
+//
+//  Created by Andrew Pouliot on 3/4/11.
+//  Copyright 2011 Darknoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageLayoutAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
